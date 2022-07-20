fc23fd739f5ecf21ccbd7d1be8c45fab69f28655
MarwaEzzatMo <marwaezzat201@gmail.com>
2022-07-21 00:46:01 +0200

    update 1

--- script.groovy
+++ script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-creden', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t marwaezzat/java-app .'
+        sh 'docker build -t marwaezzat/java-app:1.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push marwaezzat/java-app'
+        sh 'docker push marwaezzat/java-app:1.0'
     }
 } 
 
